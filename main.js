44963fb252bbd2afe13b917ed43dcda37b0adf6b
r3surr3cti0n <matraattila2004@gmail.com>
2021-05-29 16:18:51 +0200

    created important constants

---
+++ main.js
@@ -0,0 +1,4 @@
+const images = ["Kő.png", "Papír.png", "Olló.png"];
+const slider = document.querySelector(".slider");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");

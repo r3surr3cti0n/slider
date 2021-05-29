7002a536e06476d63f97bb28cd87fe8b7dc8042d
r3surr3cti0n <matraattila2004@gmail.com>
2021-05-29 16:22:22 +0200

    added one more constant

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ const images = ["Kő.png", "Papír.png", "Olló.png"];
 const slider = document.querySelector(".slider");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
+const img = document.querySelector(".slide-img");

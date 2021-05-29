ee5697072148700f87fda0ce1eddd9b560c1d9ba
r3surr3cti0n <matraattila2004@gmail.com>
2021-05-29 17:20:41 +0200

    added previous image functionality

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const img = document.querySelector(".slide-img");
 
-// prevBtn.addEventListener("click", PrevImg);
+prevBtn.addEventListener("click", PrevImg);
 nextBtn.addEventListener("click", NextImg);
 
 // Store current image name
@@ -20,12 +20,27 @@ slider.innerHTML = `<img src="img/${images[currentImgIndex].img}">`; // Kő.png
 function NextImg() {
 	// If the current index is not equal to the images object's length
 	// then increase the current index.
-	// Else set the current index to 0 so we can start from the beginning.
+	// Else set the current index to 0 so we can start from the beginning,
+	// so the next image is going to be the first image.
 	if (currentImgIndex !== images.length - 1) {
 		currentImgIndex++;
 		slider.innerHTML = `<img src="img/${images[currentImgIndex].img}">`;
 	} else {
 		currentImgIndex = 0;
+		slider.innerHTML = `<img src="img/${images[currentImgIndex].img}">`;
+	}
+}
+
+function PrevImg() {
+	// If the current index is not equal to zero then decrement the
+	// current index.
+	// Else set the current index equal to the last index,
+	// so the previous image is going to be the last image.
+	if (currentImgIndex !== 0) {
+		currentImgIndex--;
+		slider.innerHTML = `<img src="img/${images[currentImgIndex].img}">`;
+	} else {
+		currentImgIndex = images.length - 1;
 		slider.innerHTML = `<img src="img/${images[currentImgIndex].img}">`;
 	}
 }

bcd59449becbf09b856b62e4865f03bc5c8dc74e
r3surr3cti0n <matraattila2004@gmail.com>
2021-05-29 16:37:33 +0200

    now we know the current Img's index

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ const slider = document.querySelector(".slider");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const img = document.querySelector(".slide-img");
+
+// prevBtn.addEventListener("click", PrevImg);
+// nextBtn.addEventListener("click", NextImg);
+
+let currentImg = img.getAttribute("src");
+let currentImgName = currentImg.slice(4, currentImg.length);
+let currentImgIndex = () => {
+	for (let i = 0; i < images.length; i++) {
+		if (images[i] === currentImgName) {
+			return i;
+		}
+	}
+};
+
+// function NextImg() {}
+// function PrevImg(params) {}
